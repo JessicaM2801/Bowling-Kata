a5194cf6d7a02f9f797c32944de47af4c42075c3
JessicaM2801 <45536028+JessicaM2801@users.noreply.github.com>
2018-12-04 00:45:43 +0100

    Add files via upload

--- BowlingKata/src/Game.java
+++ BowlingKata/src/Game.java
@@ -26,10 +26,26 @@ public class Game {
 	public int calculateScores() {
 		int score = 0;
 		ArrayList<Integer> rolls = new ArrayList<Integer>();
-		boolean hitAllPins = false;
-		for(int i=0; i<frames.size(); i++) {
+		for(int i=0; i<frames.size()-1; i++) {
 			rolls = frames.get(i).getRolls();
-			
+			for(int roll: rolls) {
+				score += roll;
+			}
+			if(rolls.get(0) == 10) {
+				rolls = frames.get(i+1).getRolls();
+				score += rolls.get(0) + rolls.get(1);
+				if(rolls.size() == 2 && rolls.get(0) == 10 && rolls.get(1) == 0) {
+					score += frames.get(i+2).getRolls().get(0);
+				}
+			}
+			else if(rolls.get(0)+rolls.get(1) == 10) {
+				rolls = frames.get(i+1).getRolls();
+				score += rolls.get(0);
+			}
+		}
+		rolls = frames.get(frames.size()-1).getRolls();
+		for(int roll: rolls) {
+			score += roll;
 		}
 		return score;
 	}
