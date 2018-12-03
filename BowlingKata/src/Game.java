246f7de75858804a8cbd48620df2929fe9ffc0ea
JessicaM2801 <45536028+JessicaM2801@users.noreply.github.com>
2018-12-03 21:17:23 +0100

    Add files via upload

--- BowlingKata/src/Game.java
+++ BowlingKata/src/Game.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
 
 public class Game {
-
+	
+	private ArrayList<Frame> frames;
+	
+	public Game() {
+		frames = new ArrayList<Frame>();
+	}
+	
+	public void addFrame(int roll1, int roll2) {
+		Frame toAdd = new Frame();
+		toAdd.addRoll(roll1);
+		toAdd.addRoll(roll2);
+		frames.add(toAdd);
+	}
+	
+	public void addFrame(int roll1, int roll2, int roll3) {
+		Frame toAdd = new Frame();
+		toAdd.addRoll(roll1);
+		toAdd.addRoll(roll2);
+		toAdd.addRoll(roll3);
+		frames.add(toAdd);
+	}
+	
+	public int calculateScores() {
+		int score = 0;
+		ArrayList<Integer> rolls = new ArrayList<Integer>();
+		boolean hitAllPins = false;
+		for(int i=0; i<frames.size(); i++) {
+			rolls = frames.get(i).getRolls();
+			
+		}
+		return score;
+	}
 }
--- BowlingKata/test/GameTest.java
+++ BowlingKata/test/GameTest.java
@@ -1,46 +1,76 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
 
 	@Test
 	void testAllMiss() {
-		fail("Not yet implemented");
+		Game myGame = new Game();
+		for(int i=0; i<9; i++)
+			myGame.addFrame(0,0);
+		myGame.addFrame(0,0,0);
+		assertEquals("Alle Würfe daneben, Punkte: 0", 0, myGame.calculateScores());
 	}
 	
 	@Test
 	void testAllStrike() {
-		fail("Not yet implemented");
+		Game myGame = new Game();
+		for(int i=0; i<9; i++)
+			myGame.addFrame(10,0);
+		myGame.addFrame(10,10,10);
+		assertEquals("Alle Würfe Strike, Punkte: 300", 300, myGame.calculateScores());
 	}
 	
 	@Test
 	void testAllSpare() {
-		fail("Not yet implemented");
+		Game myGame = new Game();
+		for(int i=0; i<9; i++)
+			myGame.addFrame(8,2);
+		myGame.addFrame(8,2,8);
+		assertEquals("Alle Würfe Spare, Punkte: 180", 180, myGame.calculateScores());
 	}
 	
 	@Test
 	void testAll2PinsDown() {
-		fail("Not yet implemented");
+		Game myGame = new Game();
+		for(int i=0; i<10; i++)
+			myGame.addFrame(2,2);
+		assertEquals("Alle Würfe 2 getroffen, Punkte: 40", 40, myGame.calculateScores());
 	}
 	
 	@Test
 	void test1StrikeRest2PinsDown() {
-		fail("Not yet implemented");
+		Game myGame = new Game();
+		myGame.addFrame(10,0);
+		for(int i=0; i<9; i++)
+			myGame.addFrame(2,2);
+		assertEquals("1 Strike, alle anderen Würfe 2, Punkte: 50", 50, myGame.calculateScores());
 	}
 	
 	@Test
 	void testLastStrikeRest2PinsDown() {
-		fail("Not yet implemented");
+		Game myGame = new Game();
+		for(int i=0; i<9; i++)
+			myGame.addFrame(2,2);
+		myGame.addFrame(10,2,2);
+		assertEquals("Letzter Wurf Strike, alle anderen Würfe 2, Punkte: 50", 50, myGame.calculateScores());
 	}
 	
 	@Test
 	void test1SpareRest2PinsDown() {
-		fail("Not yet implemented");
+		Game myGame = new Game();
+		myGame.addFrame(8,2);
+		for(int i=0; i<9; i++)
+			myGame.addFrame(2,2);
+		assertEquals("Erster Frame Spare, alle anderen Würfe 2, Punkte: 48", 48, myGame.calculateScores());
 	}
 	
 	@Test
 	void testLastSpareRest2PinsDown() {
-		fail("Not yet implemented");
+		Game myGame = new Game();
+		for(int i=0; i<9; i++)
+			myGame.addFrame(2,2);
+		myGame.addFrame(8,2,2);
+		assertEquals("Letzter Wurf Spare, alle anderen Würfe 2, Punkte: 48", 48, myGame.calculateScores());
 	}
 }
